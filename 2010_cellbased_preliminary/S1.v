4ea3000d400687c55f5698d6af473c69ea344bfd
JKao <fireeagle5408@gmail.com>
2021-11-04 23:30:17 +0800

    Fix Latch

--- 2010_cellbased_preliminary/S1.v
+++ 2010_cellbased_preliminary/S1.v
@@ -13,63 +13,105 @@ reg [4:0] RB1_A;
 reg [7:0] RB1_D;
 reg sen, sd;
 //----------------------------------------------------------------------------
-reg [1:0] k, state;
+reg [1:0] k;
 reg [2:0] j;
+reg [2:0] cs, ns;
 reg [4:0] i;
 reg [7:0] mem [17:0];
 //----------------------------------------------------------------------------
-parameter read_RB1 = 2'b00, send_addr = 2'b01, send_data = 2'b10, pending = 2'b11;
+parameter IDLE = 3'd0,
+          READ_RB1 = 3'd1,
+          SEND_ADDR = 3'd2,
+          SEND_DATA = 3'd3,
+          PEND = 3'd4;
 //----------------------------------------------------------------------------
-always@(negedge clk or posedge rst) begin
-      if(rst) begin
-            RB1_RW = 1; sen = 1;
-            RB1_A = 0; RB1_D = 0; sd = 0;
 
-            i = 0; j = 0; k = 2; state = read_RB1;
-      end else begin
-            case(state)
-                  read_RB1: begin
-                        if(RB1_Q >= 0) begin
-                              mem[i] = RB1_Q;
-                              i = i + 1;
-                              RB1_A = i;
-                              state = read_RB1;
-                        end else state = send_addr;
-                  end
-                  send_addr: begin
-                        sen = 0;
-                        if(k == 0) begin
-                              sd = j[k];
-                              i = 17;
-                              k = 2;
-                              state = send_data;
-                        end else begin
-                              sd = j[k];
-                              k = k - 1;
-                              state = send_addr;
-                        end
-                  end
-                  send_data: begin
-                        sen = 0;
-                        sd = mem[i][7 - j];
-                        if(i == 0) state = pending;
-                        else begin
-                              i = i - 1;
-                              state = send_data;
-                        end
-                  end
-                  pending: begin
-                        sen = 1;
-                        if(j == 7) begin
-                              state = pending;
-                        end else begin
-                              j = j + 1;
-                              state = send_addr;
-                        end
+always @(negedge clk or posedge rst) begin
+      if(rst) cs <= IDLE;
+      else cs <= ns;
+end
 
-                  end
-            endcase
+always @(*) begin
+      case (cs)
+            IDLE: ns = READ_RB1;
+            READ_RB1: begin
+                  if(RB1_Q >= 0) ns = READ_RB1;
+                  else ns = SEND_ADDR;
+            end
+            SEND_ADDR: begin
+                  if(k == 2'd0) ns = SEND_DATA;
+                  else ns = SEND_ADDR;
+            end
+            SEND_DATA: begin
+                  if(i == 5'd0) ns = PEND;
+                  else ns = SEND_DATA;
+            end
+            PEND: begin
+                  if(j == 3'd7) ns = PEND;
+                  else ns = SEND_ADDR;
+            end
+            default: ns = IDLE;
+      endcase
+end
+
+// mem
+always @(negedge clk or posedge rst) begin
+      if(cs == READ_RB1) begin
+            mem[i] <= RB1_Q;
       end
 end
+
+// RB1_RW
+always @(negedge clk or posedge rst) begin
+      if(rst) RB1_RW <= 1'd1;
+end
+
+// RB1_A
+always @(negedge clk or posedge rst) begin
+      if(rst) RB1_A <= 5'd0;
+      else if(cs == READ_RB1)  RB1_A <= RB1_A + 5'd1;
+end
+
+// RB1_D
+always @(*) begin
+      if(cs == IDLE) RB1_D = 8'd0;
+      else RB1_D = 8'd0;
+end
+
+// i
+always @(negedge clk or posedge rst) begin
+      if(rst) i <= 5'd0;
+      else if(cs == READ_RB1) i <= i + 5'd1;
+      else if(cs == SEND_ADDR) i <= (k == 2'd0) ? 5'd17 : i;
+      else if(cs == SEND_DATA) i <= i - 5'd1;
+end
+
+// k
+always @(negedge clk or posedge rst) begin
+      if(rst) k <= 2'd2;
+      else if(cs == SEND_ADDR) k <= (k == 2'd0) ? 2'd2 : k - 2'd1;
+end
+
+// sd
+always @(*) begin
+      if(rst) sd = 1'd0;
+      else if(cs == SEND_ADDR) sd = j[k];
+      else if(cs == SEND_DATA) sd = mem[i][7 - j];
+      else sd = 1'd0;
+end
+
+// j
+always @(negedge clk or posedge rst) begin
+      if(rst) j <= 3'd0;
+      else if(cs == PEND) j <= j + 3'd1;
+end
+
+// sen
+always @(*) begin
+      if(cs == IDLE) sen = 1'd1;
+      else if(cs == SEND_ADDR || cs == SEND_DATA) sen = 1'd0;
+      else sen = 1'd1;
+end
+
 //----------------------------------------------------------------------------
 endmodule
--- 2010_cellbased_preliminary/S2.v
+++ 2010_cellbased_preliminary/S2.v
@@ -6,47 +6,101 @@ output S2_done, RB2_RW;
 output [2:0] RB2_A;
 output [17:0] RB2_D;
 //----------------------------------------------------------------------------
-reg S2_done, RB2_RW, finish_sign;
+reg S2_done, RB2_RW;
 reg [2:0] RB2_A;
 reg [17:0] RB2_D;
 //----------------------------------------------------------------------------
+reg [2:0] cs, ns;
 reg [2:0] addr;
 reg [3:0] k;
 reg [4:0] i;
 reg [17:0] data;
 //----------------------------------------------------------------------------
-always@(posedge clk or posedge rst) begin
-	if(rst) begin
-		RB2_RW = 1;
-		RB2_A = 0; RB2_D = 0; S2_done = 0;
+parameter IDLE = 3'd0,
+          GET_ADDR = 3'd1,
+		  GET_DATA = 3'd2,
+          SAVE_DATA = 3'd3,
+		  DONE = 3'd4;
+//----------------------------------------------------------------------------
 
-		i = 0; k = 0; finish_sign = 0;
-	end else begin
-		if(!sen) begin
-			RB2_RW = 1;
-			if(i >= 3) begin
-				data[20 - i] = sd;
-				i = i + 1;
-				if(i == 21) begin
-					RB2_RW = 0;
-					RB2_A = addr;
-					RB2_D = data;
-					k = k + 1;
-					i = 0;
-					if(k == 8) begin
-						finish_sign = 1;
-					end
-				end
-			end else begin
-				addr[2 - i] = sd;
-				i = i + 1;
-			end
-		end else begin
-			if(finish_sign && RB2_Q == RB2_D) begin
-				S2_done = 1;
-			end
-		end
-	end
+always@(posedge clk or posedge rst) begin
+	if(rst) cs <= IDLE;
+	else cs <= ns;
 end
+
+always@(*) begin
+	case (cs)
+		IDLE: begin
+			if(sen == 1'd0) ns = GET_ADDR;
+			else ns = IDLE;
+		end
+		GET_ADDR: begin
+			if(i < 5'd3) ns = GET_ADDR;
+			else ns = GET_DATA;
+		end
+		GET_DATA: begin
+			if(i < 5'd21) ns = GET_DATA;
+			else ns = SAVE_DATA;
+		end
+		SAVE_DATA: begin
+			if(k == 4'd7) ns = DONE;
+			else ns = GET_ADDR;
+		end
+		DONE: ns = IDLE;
+		default: ns = IDLE;
+	endcase
+end
+
+//addr
+always @(posedge clk or posedge rst) begin
+      if(ns == GET_ADDR) addr[2 - i] <= sd;
+end
+
+//data
+always @(posedge clk or posedge rst) begin
+      if(ns == GET_DATA) data[20 - i] <= sd;
+end
+
+// RB2_RW
+always @(*) begin
+      if(rst) RB2_RW = 1'd1;
+      else if(cs == SAVE_DATA) RB2_RW = 1'd0;
+	  else RB2_RW = 1'd1;
+end
+
+// RB2_A
+always @(*) begin
+      if(rst) RB2_A = 3'd0;
+      else if(cs == SAVE_DATA) RB2_A = addr;
+	  else RB2_A = 3'd0;
+end
+
+// RB2_D
+always @(*) begin
+      if(rst) RB2_D = 0;
+      else if(cs == SAVE_DATA) RB2_D = data;
+	  else RB2_D = 0;
+end
+
+// S2_done
+always @(*) begin
+      if(rst) S2_done = 1'd0;
+      else if(cs == DONE) S2_done = 1'd1;
+	  else S2_done = 1'd0;
+end
+
+// i
+always @(posedge clk or posedge rst) begin
+      if(rst) i <= 5'd0;
+      else if(ns == GET_ADDR || ns == GET_DATA) i <= i + 5'd1;
+      else if(ns == SAVE_DATA) i <= 5'd0;
+end
+
+// k
+always @(posedge clk or posedge rst) begin
+      if(rst) k <= 4'd0;
+      else if(cs == SAVE_DATA) k <= k + 4'd1;
+end
+
 //----------------------------------------------------------------------------
 endmodule
